ebf817791271b5e84a46e1a34908a6389c76f029
Civicverse Protocol Project <210203117+Civicverse@users.noreply.github.com>
2026-02-28 09:48:36 -0500

    Delete civic-id directory

--- civic-id/src/trust.ts
+++
@@ -1,9 +0,0 @@
-import { readUsers, writeUsers } from "./storage";
-
-export function vouch(civicId: string): void {
-  const users = readUsers();
-  const user = users.find(u => u.civicId === civicId);
-  if (user) user.trust += 1;
-  writeUsers(users);
-}
-
